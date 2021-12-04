4d34e42cf8673c475744cc71013b421678c07038
SUPERMECHM500 <thedestoryer11@gmail.com>
2021-12-04 15:54:34 -0500

    update example

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ from classes import MALSchedule
 
 if __name__ == "__main__":
     scheduler = MALSchedule()
-    # print(scheduler()["Thursday"][0])  # Currently Komi Can't Communicate
-    
     schedule = scheduler.request_schedule()
     for day, animes in schedule.items():
         print(f"-------------------- {day} --------------------")
@@ -14,7 +12,7 @@ if __name__ == "__main__":
                 f"Tags: {', '.join(anime.tags)}\n"
                 f"Rating: {anime.score}/10\n"
                 f"Metadata: {', '.join([key+': '+value for key, value in anime.metadata.items()])}\n"
-                f"Viewer discretion: {anime.discretion}\n"
+                f"Viewer discretion: {anime.estimated_rating}\n"
                 f"\n"
                 f"Synopsis:\n"
                 f"{anime.synopsis}\n"
@@ -22,3 +20,20 @@ if __name__ == "__main__":
                 f"----------\n"
                 f"\n"
             )
+
+    anime = schedule["Thursday"][0]  # Currently Komi Can't Communicate
+    print(
+        f"# SINGLE ANIME\n"
+        f"Name: {anime.name}\n"
+        f"Producer: {anime.producer}\n"
+        f"Tags: {', '.join(anime.tags)}\n"
+        f"Rating: {anime.score}/10\n"
+        f"Metadata: {', '.join([key+': '+value for key, value in anime.metadata.items()])}\n"
+        f"Viewer discretion: {anime.estimated_rating}\n"
+        f"\n"
+        f"Synopsis:\n"
+        f"{anime.synopsis}\n"
+        f"Image url: {anime.image_url}\n"
+        f"----------\n"
+        f"\n"
+    )
